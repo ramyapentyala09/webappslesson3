aee21e0ab33d7463ae27cb3f5e15fb9d45952230
ramyapentyala <ramyapentyala@gmail.com>
2022-02-26 12:49:24 -0600

    init

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,5 +1,5 @@
 import { 
-    getAuth, signInWithEmailAndPassword, signOut
+    getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js"
 
 import * as Elements from '../viewpage/elements.js'
@@ -7,8 +7,10 @@ import { DEV } from "../model/constants.js";
 import * as Util from '../viewpage/util.js'
 
 const auth = getAuth();
-
+export let currentUser = null;
 export function addEventListeners() {
+
+    onAuthStateChanged(auth, authStateChanged);
 
     Elements.formSignIn.addEventListener('submit', async e => {
         e.preventDefault();
@@ -35,4 +37,29 @@ Util.info('Sign out error', JSON.stringify(e));
         }
     });
 
+}
+async function authStateChanged(user) {
+currentUser = user;
+if (user) {
+let menus = document.getElementsByClassName('modal-preauth');
+for (let i = 0; i < menus.length; i++) {
+    menus[i].style.display = 'none';
+}
+menus = document.getElementsByClassName('modal-postauth');
+for (let i = 0; i < menus.length; i++) {
+    menus[i].style.display = 'block';
+}
+console.log('==== signed in');
+} else {
+let menus = document.getElementsByClassName('modal-preauth');
+for (let i = 0; i < menus.length; i++) {
+    menus[i].style.display = 'block';
+}
+menus = document.getElementsByClassName('modal-postauth');
+for (let i = 0; i < menus.length; i++) {
+    menus[i].style.display = 'none';
+}
+console.log('==== signed out');
+
+}
 }
